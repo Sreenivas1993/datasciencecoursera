bf5c7299583183663c53f8e70bf6d274d7266e17
Sreenivas1993 <sreenivenki@gmail.com>
2017-07-18 08:43:07 -0500

    commit

---
+++ Making and Cleaning Data Assignment/README.md
@@ -0,0 +1,10 @@
+Getting and cleaning data assignment
+
+run_analysis.R does the following steps
+
+1.Download the dataset in the working directory
+2.merge the test and train dataset into one dataset
+3.read the feature information to 'features' dataset and take yhe columns which have means and standard deviation in it.
+4.Read the activity information to'activity' dataset and replace the activity number with activity name
+5.Given proper column names to all dataset and all data set are merged using'cbind'
+6.Have taken averages of all columns other than activities and subject grouped by these two columns and this tidy data set is stored in 'tidydata.txt'
---
+++ Making and Cleaning Data Assignment/run_analysis.R
@@ -0,0 +1,55 @@
+
+library(plyr)
+library(reshape2)
+
+filename <- "getdata_dataset.zip"
+
+## Download and unzip the dataset:
+if (!file.exists(filename)){
+  fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip "
+  download.file(fileURL, filename, method="curl")
+}  
+if (!file.exists("UCI HAR Dataset")) { 
+  unzip(filename) 
+}
+
+###1.#####
+
+x_train<-read.table("UCI HAR Dataset/train/X_train.txt")
+y_train<-read.table("UCI HAR Dataset/train/y_train.txt")
+subject_train<-read.table("UCI HAR Dataset/train/subject_train.txt")
+x_test<-read.table("UCI HAR Dataset/test/X_test.txt")
+y_test<-read.table("UCI HAR Dataset/test/y_test.txt")
+subject_test<-read.table("UCI HAR Dataset/test/subject_test.txt")
+subject_train<-read.table("UCI HAR Dataset/train/subject_train.txt")
+x_data<-rbind(x_test,x_train)
+y_data<-rbind(y_test,y_train)
+subject_data<-rbind(subject_test,subject_train)
+
+
+###2.###
+features<-read.table("UCI HAR Dataset/features.txt")
+meanstd<-grep(".*mean.*|.*std.*",features[,2])
+x_data<-x_data[,meanstd]
+names(x_data)<-features[meanstd,2]
+
+
+####3.#########
+
+activity<-read.table("UCI HAR Dataset/activity_labels.txt")
+y_data[,1]<-activity[y_data[,1],2]
+names(y_data)<-"Activities"
+
+
+############.4####################
+names(subject_data)<-"Subject"
+totaldat<-cbind(x_data,y_data,subject_data)
+
+
+################3.5###################
+
+#####totaldat<-as.data.table(totaldat)###
+###avg_data<-totaldat[,lapply(.SD,mean),by=.(Activities,Subject)]###
+
+averages_data <- ddply(totaldat, .(Subject,Activities), function(x) colMeans(x[, 1:66]))
+write.table(averages_data,"tidydata.txt",row.names = FALSE,quote = FALSE)
